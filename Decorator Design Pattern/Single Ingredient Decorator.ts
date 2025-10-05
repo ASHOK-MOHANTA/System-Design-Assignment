8df773114a850ccdb5a110d5154bec46c1f11179
ASHOK-MOHANTA <subendum25@gmail.com>
2025-10-05 16:46:50 +0530

    Assignment

---
+++ Decorator Design Pattern/Single Ingredient Decorator.ts
@@ -0,0 +1,45 @@
+// Abstract Class
+
+abstract class Beverage{
+    abstract getDescription(): string;
+    abstract getCost(): Number;
+}
+
+//Concreat Class
+class GreenTea extends Beverage{
+    getDescription(): string {
+        return "Green Tea"
+    };
+
+    getCost(): Number {
+        return 40;
+    }
+}
+
+// Decorator Base Class
+
+abstract class BeverageDecorater extends Beverage{
+    protected beverage : Beverage;
+
+    constructor(beverage : Beverage){
+        super();
+        this.beverage = beverage;
+    }
+    abstract getDescription(): string;
+    abstract getCost(): Number;
+}
+
+// class Decorator
+
+class Sugar extends BeverageDecorater{
+    getDescription(): string {
+        return this.beverage.getDescription()+"+ Sugar";
+    }
+    getCost(): Number {
+        return this.beverage.getCost() + 10;
+    }
+}
+
+const tea = new Sugar(new GreenTea());
+console.log(tea.getDescription()); 
+console.log(tea.getCost());
